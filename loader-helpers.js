0e7cfb651f5af5d621eee30e74be0dc5fa415faf
Sabrina <sabreyes01@gmail.com>
2025-11-14 03:59:55 -0500

    fix: don't let horse gallop far

--- loader-helpers.js
+++ loader-helpers.js
@@ -4,11 +4,15 @@ const LIGHT_BROWN = '\x1b[38;5;180m';
 const RESET = '\x1b[0m';
 const MOVE_CURSOR_HOME = '\x1b[H';
 const CLEAR_FROM_CURSOR = '\x1b[J';
+const MAX_SCREEN_WIDTH = 40;
 
 function startLoader() {
     let i = 0;
 
     return setInterval(() => {
+        if (i > MAX_SCREEN_WIDTH) {
+            i = 0;
+        }
         process.stdout.write(MOVE_CURSOR_HOME);
         process.stdout.write(CLEAR_FROM_CURSOR);
         const frame = gallop[i % gallop.length].map(line => ' '.repeat(i) + line).join('\n');
