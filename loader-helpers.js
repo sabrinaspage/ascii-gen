dc791419667d6a704233595f3f3ce19a3f633e8a
Sabrina <sabreyes01@gmail.com>
2025-11-14 02:53:04 -0500

    feat: ascii horse animation

--- loader-helpers.js
+++ loader-helpers.js
@@ -1,16 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const LIGHT_BROWN = '\x1b[38;5;180m';
+const RESET = '\x1b[0m';
+const STATIC_ASCII_DIR = 'loading-ascii';
+const MOVE_CURSOR_HOME = '\x1b[H';
+const CLEAR_FROM_CURSOR = '\x1b[J';
+
+const staticDir = path.join(__dirname, STATIC_ASCII_DIR);
+const files = fs.readdirSync(staticDir).map(file => {
+    return fs.readFileSync(path.join(staticDir, file), 'utf-8');
+});
+
 function startLoader() {
-    const frames = ['~^~^~^~', '^~^~^~^'];
+    const frames = [...files];
     let i = 0;
 
     return setInterval(() => {
-        process.stdout.write(`\r${frames[i % frames.length]}`.repeat(i));
+        process.stdout.write(MOVE_CURSOR_HOME);
+        process.stdout.write(CLEAR_FROM_CURSOR);
+        process.stdout.write(LIGHT_BROWN + frames[i % frames.length] + RESET);
         i++;
-    }, 200); // change every 200ms
+    }, 200);
 }
 
 function stopLoader(interval) {
     clearInterval(interval);
-    process.stdout.write('\r\x1b[K'); // clears the line
+    process.stdout.write(MOVE_CURSOR_HOME + CLEAR_FROM_CURSOR);
 }
 
 module.exports = { startLoader, stopLoader };
